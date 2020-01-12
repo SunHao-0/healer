2272adbd4f1d0eda5bd6cd141c0109279f8fe75b
sam <sunhaoyl@outlook.com>
2020-01-12 18:06:44 +0800

    Add fots bin. Add support for cli interface

--- fots/src/lib.rs
+++ fots/src/lib.rs
@@ -1,3 +1,25 @@
+//! Fots
+//!
+//! Fots is a fuzzing oriented type system used as system call description.
+//! It has part of rust's type and type constructor with golang likely syntex.
+//! A fots file consists four kind of items: type def, func def, group def and rule def.
+//! Every func belongs to a group. A sample example as follow:
+//! ```
+//! type fd = res<i32>
+//! struct stat{...}
+//! flag statx_flags { xx = 0x0 }
+//! type statx_mask = u64{0x001,0x002}
+//!
+//! group FileStat{
+//!     fn stat(file *In cstr, statbuf *Out stat)
+//!     fn lstat(file *In cstr, statbuf *Out stat)
+//!     fn fstat(fd Fd, statbuf *Out stat)
+//!     fn newfstatat(dfd i32{0}, file *In cstr, statbuf *Out stat, f statx_flags)
+//!     fn statx(fd Fd, file *In cstr, flags statx_flags, mask statx_mask, statxbuf *Out statx)
+//! }
+//}
+//! ```
+//!
 #[macro_use]
 extern crate maplit;
 extern crate num_traits;
@@ -8,12 +30,16 @@ extern crate prettytable;
 #[cfg(test)]
 #[macro_use]
 extern crate runtime_fmt;
+extern crate structopt;
+#[macro_use]
+extern crate thiserror;
 
 use pest::iterators::Pairs;
 use pest::Parser;
 
 use grammar::{GrammarParser, Rule};
 
+pub mod errors;
 pub mod grammar;
 pub mod items;
 mod num;
@@ -46,6 +72,6 @@ pub fn parse_grammar(text: &str) -> Result<Pairs<Rule>, pest::error::Error<Rule>
 /// let mut re = parse_items(text);
 /// assert!(re.is_ok());
 /// ```
-pub fn parse_items(text: &str) -> Result<types::Items, pest::error::Error<Rule>> {
+pub fn parse_items(text: &str) -> Result<types::Items, errors::Error> {
     items::parse(text)
 }
