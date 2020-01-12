0e6e1ef20ed86cf45511373dadff4ce9c5f2a91e
sam <sunhaoyl@outlook.com>
2020-01-12 18:01:53 +0800

    fix number parse

--- fots/src/num.rs
+++ fots/src/num.rs
@@ -1,17 +1,22 @@
 use num_traits::Num;
 
+/// Parse number value from string literal.
 pub fn parse<T: Num>(input: &str) -> Result<T, T::FromStrRadixErr> {
-    let input = input.trim();
+    let mut input = input.trim();
+    assert!(!input.is_empty());
+    assert_ne!(&input[0..1], "+");
+
+    let mut sign = '+';
+    if input.starts_with('-') {
+        input = &input[1..];
+        sign = '-';
+    }
     if input.starts_with("0x") | input.starts_with("0X") {
-        return T::from_str_radix(&input[2..], 16);
+        return T::from_str_radix(&format!("{}{}", sign, &input[2..]), 16);
     }
-
     if input.starts_with("0b") || input.starts_with("0B") {
-        return T::from_str_radix(&input[2..], 2);
+        return T::from_str_radix(&format!("{}{}", sign, &input[2..]), 2);
     }
-    //    if input.starts_with("0") {
-    //        return T::from_str_radix(input.trim_start_matches("0"), 8);
-    //    }
 
-    T::from_str_radix(input, 10)
+    T::from_str_radix(&format!("{}{}", sign, input), 10)
 }
