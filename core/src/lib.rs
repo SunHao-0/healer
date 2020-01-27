1da63b3cacd63000cd1cd922de3753741fb37516
sam <sunhaoyl@outlook.com>
2020-01-27 20:01:05 +0800

    Add min, mutate module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,8 @@ extern crate maplit;
 pub mod analyze;
 pub mod c;
 pub mod gen;
+pub mod minimize;
+pub mod mutate;
 pub mod prog;
 pub mod target;
 pub mod value;
---
+++ core/src/mutate.rs
@@ -0,0 +1,6 @@
+use crate::prog::Prog;
+use crate::target::Target;
+
+pub fn mutate(_p: &Prog, _t: &Target) -> Prog {
+    todo!()
+}
