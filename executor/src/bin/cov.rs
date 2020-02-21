039714f89895961d12baaea861bf74f47d09007f
sam <sunhaoyl@outlook.com>
2020-02-21 22:01:14 +0800

    collect with kcov and running with picoc

--- executor/src/bin/cov.rs
+++ executor/src/bin/cov.rs
@@ -4,14 +4,12 @@ use executor::picoc::Picoc;
 fn main() {
     let mut handle = cover::open();
     let mut pc = Picoc::default();
+    let prog = "char buf[64];
+                read(0,buf,64);"
+        .into();
 
     let pc = handle.collect(|| {
-        if !pc.execute(String::from(
-            "\
-    char buf[64];
-    read(0,buf,64);
-    ",
-        )) {
+        if !pc.execute(prog) {
             eprintln!("Failed");
         }
     });
