c360e31dda16f1f6d502c6d8549c4e2227a4e298
sam <sunhaoyl@outlook.com>
2020-02-24 21:55:35 +0800

    Add block, branch type

--- fuzzer/src/feedback.rs
+++ fuzzer/src/feedback.rs
@@ -1,37 +1,77 @@
-use executor::Branch;
 use std::collections::HashSet;
+use std::iter::Extend;
 use tokio::sync::Mutex;
+
+#[derive(Clone, Debug, Default, Hash, PartialOrd, PartialEq, Ord, Eq)]
+pub struct Block(usize);
+
+impl From<usize> for Block {
+    fn from(raw: usize) -> Self {
+        Self(raw)
+    }
+}
+
+#[derive(Clone, Debug, Default, Hash, PartialOrd, PartialEq, Ord, Eq)]
+pub struct Branch(usize);
+
+impl From<(Block, Block)> for Branch {
+    fn from((b1, b2): (Block, Block)) -> Self {
+        let b1 = b1.0 >> 1;
+        Self(b1 ^ b2.0)
+    }
+}
 
 #[derive(Default)]
 pub struct FeedBack {
     branches: Mutex<HashSet<Branch>>,
+    blocks: Mutex<HashSet<Block>>,
 }
 
 impl FeedBack {
-    pub async fn merge(&self, branches: Vec<Branch>) -> Option<Vec<Branch>> {
-        let mut inner = self.branches.lock().await;
+    pub async fn diff_branch(&self, branches: &[Branch]) -> HashSet<Branch> {
+        let inner = self.branches.lock().await;
 
-        let mut new_branches = Vec::new();
-        for b in branches.into_iter() {
-            if !inner.contains(&b) {
-                new_branches.push(b.clone());
-                inner.insert(b);
+        let mut result = HashSet::new();
+        for b in branches {
+            if !inner.contains(b) {
+                result.insert(b.clone());
             }
         }
-        if new_branches.is_empty() {
-            None
-        } else {
-            Some(new_branches)
+        result.shrink_to_fit();
+        result
+    }
+
+    pub async fn diff_block(&self, blocks: &[Block]) -> HashSet<Block> {
+        let inner = self.blocks.lock().await;
+
+        let mut result = HashSet::new();
+        for b in blocks {
+            if !inner.contains(b) {
+                result.insert(b.clone());
+            }
+        }
+        result.shrink_to_fit();
+        result
+    }
+
+    pub async fn merge(&self, blocks: HashSet<Block>, branches: HashSet<Branch>) {
+        {
+            let mut inner = self.branches.lock().await;
+            inner.extend(branches);
+        }
+        {
+            let mut inner = self.blocks.lock().await;
+            inner.extend(blocks);
         }
     }
 
-    pub async fn len(&self) -> usize {
+    pub async fn branch_len(&self) -> usize {
         let b = self.branches.lock().await;
         b.len()
     }
 
-    pub async fn is_empty(&self) -> bool {
-        let b = self.branches.lock().await;
-        b.is_empty()
+    pub async fn block_len(&self) -> usize {
+        let b = self.blocks.lock().await;
+        b.len()
     }
 }
