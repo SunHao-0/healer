411e5cab4a474705a2812f13326dea4d53a69695
sam <sunhaoyl@outlook.com>
2020-03-12 11:10:23 +0800

    Add translate flag

--- tools/src/bin/gen.rs
+++ tools/src/bin/gen.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use std::process::exit;
 use structopt::StructOpt;
 use tools::load_target;
+use core::{c, gen, analyze};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "gen", about = "Generate progs standard output")]
@@ -10,24 +11,30 @@ struct Settings {
     /// Fots target
     #[structopt(long, short = "i")]
     items: PathBuf,
+
+    #[structopt(long, short = "t")]
+    translate: bool,
 }
 
 fn main() {
     let settings = Settings::from_args();
 
     let target = load_target(&settings.items);
+    let rt = analyze::static_analyze(&target);
+    let p = gen::gen(&target, &rt, &Default::default());
 
-    let rt = core::analyze::static_analyze(&target);
+    if settings.translate {
+        let p = c::to_prog(&p, &target);
+        println!("{}", p);
+    } else {
+        let p = bincode::serialize(&p).unwrap_or_else(|e| {
+            eprintln!("Fail to serialize: {}", e);
+            exit(exitcode::SOFTWARE)
+        });
 
-    let p = core::gen::gen(&target, &rt, &Default::default());
-
-    let p = bincode::serialize(&p).unwrap_or_else(|e| {
-        eprintln!("Fail to serialize: {}", e);
-        exit(exitcode::SOFTWARE)
-    });
-
-    stdout().write_all(&p).unwrap_or_else(|e| {
-        eprintln!("Fail to Write: {}", e);
-        exit(exitcode::IOERR)
-    })
+        stdout().write_all(&p).unwrap_or_else(|e| {
+            eprintln!("Fail to Write: {}", e);
+            exit(exitcode::IOERR)
+        })
+    }
 }
