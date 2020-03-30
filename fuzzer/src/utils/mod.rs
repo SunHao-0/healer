9eaf7a728cc6002a25405a5d79effc4b489381c5
sam <sunhaoyl@outlook.com>
2020-03-30 17:15:14 +0800

    Add transaction loop

--- fuzzer/src/utils/mod.rs
+++ fuzzer/src/utils/mod.rs
@@ -2,3 +2,31 @@ pub mod cli;
 pub mod process;
 pub mod queue;
 pub mod split;
+
+use std::future::Future;
+use tokio::sync::broadcast;
+
+pub async fn transaction_loop<T, F, Z>(
+    mut stop_signal: broadcast::Receiver<()>,
+    clean_up: T,
+    mut transaction_gen: F,
+) where
+    T: Future,
+    Z: Future,
+    F: FnMut() -> Z,
+{
+    use broadcast::TryRecvError::*;
+
+    loop {
+        match stop_signal.try_recv() {
+            Ok(_) => {
+                clean_up.await;
+                break;
+            }
+            Err(Empty) => (),
+            Err(e) => panic!("Unexpected braodcast receiver state: {}", e),
+        }
+        let transaction = transaction_gen();
+        transaction.await;
+    }
+}
