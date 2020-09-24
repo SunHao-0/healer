92c1466e0c692cd8e8c89b46f796832ed74f99b5
SunHao-0 <sunhao@outlook.com>
2020-09-24 14:01:09 +0000

    Prepare to refactor and strengthen old dsl

---
+++ hl-spec/lib.rs
@@ -0,0 +1,17 @@
+//! Implementation of Healer-spec
+//! Healer-spec is system call specification language of healer.
+//! It is a domain specific language for kernel scenario.
+
+mod parse;
+
+pub fn parse() {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn hello_hl_spec() {
+        assert!(true);
+    }
+}
