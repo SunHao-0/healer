4805a92e3fbbf7eb269237fabfb1f82f2676413f
sam <sunhaoyl@outlook.com>
2020-09-28 10:44:34 +0800

    refactor error module

--- hl-spec/error.rs
+++ hl-spec/error.rs
@@ -1,36 +1,90 @@
 //! Error type that may be raised while parsing
 
 use crate::parse::Span;
-use std::error;
-use std::fmt;
 
-#[derive(Debug, PartialEq)]
-pub struct Error<'a> {
-    pub span: Span<'a>,
+#[derive(Debug, PartialEq, Default)]
+pub struct ParseError {
+    pub err_snippets: String,
+    pub location: (usize, usize),
     pub expect: String,
     pub found: String,
-    pub context: Option<String>,
+    pub context: Vec<String>,
+    pub filename: Option<String>,
 }
 
-impl<'a> Error<'a> {
-    pub fn add_context(source: Self, context: String) -> Self {
-        let context = if let Some(old_context) = source.context {
-            format!("{}: {}", old_context, context)
-        } else {
-            context
-        };
-
+impl ParseError {
+    pub fn new(span: Span) -> Self {
+        let err_line = span.fragment().lines().next().unwrap_or_default();
         Self {
-            context: Some(context),
-            ..source
+            err_snippets: err_line.to_string(),
+            location: (span.location_line() as usize, span.location_offset()),
+            ..Default::default()
         }
+    }
+
+    pub fn err_snippets<T: Into<String>>(mut self, snippets: T) -> Self {
+        self.err_snippets = snippets.into();
+        self
+    }
+
+    pub fn expect<T: Into<String>>(mut self, msg: T) -> Self {
+        self.expect = msg.into();
+        self
+    }
+
+    pub fn found<T: Into<String>>(mut self, msg: T) -> Self {
+        self.found = msg.into();
+        self
+    }
+
+    pub fn add_context<T: Into<String>>(mut self, ctx: T) -> Self {
+        self.context.push(ctx.into());
+        self
+    }
+
+    pub fn filename<T: Into<String>>(mut self, f: T) -> Self {
+        self.filename = Some(f.into());
+        self
     }
 }
 
-impl<'a> error::Error for Error<'a> {}
+impl Into<nom::Err<ParseError>> for ParseError {
+    fn into(self) -> nom::Err<ParseError> {
+        nom::Err::Error(self)
+    }
+}
 
-impl<'a> fmt::Display for Error<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(filename) = self.filename.as_ref() {
+            write!(f, "In {}:", filename)?;
+        }
+        write!(f, "{}:{}: ERROR: ", self.location.0, self.location.1)?;
+        for ctx in self.context.iter() {
+            write!(f, "in {}: ", ctx)?;
+        }
+        writeln!(f, "expect {}, but found {}.", self.expect, self.found)?;
+        writeln!(f, "\t{}", self.err_snippets)
+    }
+}
+
+impl std::error::Error for ParseError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn error_fmt() {
+        let span = Span::new("fn foo()\n");
+        let mut err = ParseError::new(span)
+            .filename("test.hl")
+            .add_context("test")
+            .expect("test")
+            .found("test");
+        assert_eq!(
+            "In test.hl:1:0: ERROR: in test: expect test, but found test.\n\tfn foo()\n",
+            err.to_string()
+        );
     }
 }
